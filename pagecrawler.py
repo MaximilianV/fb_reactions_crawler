13143db229a70744c1be3bd59ab1aad48f76ebcd
MaximilianV <maximilian.voelker@mailbox.org>
2017-12-04 18:48:29 +0100

    page id must be string for api usage

--- pagecrawler.py
+++ pagecrawler.py
@@ -3,7 +3,7 @@ import facebook
 
 class PageCrawler:
     def __init__(self, access_token, page_id):
-        self.page_id = page_id
+        self.page_id = str(page_id)
         self.fb_graph = facebook.GraphAPI(access_token=access_token, version="2.7")
 
     def get_latest_posts(self, count=10):
