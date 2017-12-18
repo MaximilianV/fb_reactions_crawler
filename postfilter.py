10aa42b885a265ebf1bf2eda90841af1068a186a
Christian Friedow <christian@friedow.com>
2017-12-18 11:22:59 +0100

    Added 2 filter mechanisms

--- postfilter.py
+++ postfilter.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+import re
+from functools import reduce
 
 
 def parse_arguments():
@@ -9,11 +11,29 @@ def parse_arguments():
 
 
 def post_is_useful(post):
+    # Filter posts including URLs
+    url_regex = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    if re.match(url_regex, post.message):
+        return False
+    
+    # Filter posts where the highest reaction is not dominant. 
+    # Default: 10% higher then the secondary reaction.
+    # TODO: make percentage a command line argument
+    reactions = post.reactions.values()
+    highgest_reaction = max(reactions)
+    reactions.remove(highgest_reaction)
+    second_highgest_reaction = max(reactions)
+    if (100 - 100 * second_highgest_reaction / highgest_reaction) < 10:
+        return False
+
+    # TODO: Filter posts with less than X reactions
+    # TODO: Filter posts with less than X characters
+
     return True
 
 
 def clean_post_data(posts):
-    return list(filter(post_is_useful, posts))
+    return list(filter(post_is_useful, posts.values()))
 
 
 def main(run_args):
@@ -21,7 +41,9 @@ def main(run_args):
     posts = None
     with open(filename + '.json', 'r') as infile:
         posts = json.load(infile)
-    clean_post_data(posts)
+    clean_posts = clean_post_data(posts)
+    # test dump
+    print(json.dumps(clean_posts, sort_keys=True, indent=4))
 
 
 if __name__ == "__main__":
