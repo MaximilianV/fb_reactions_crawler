6842b5ee3ad7cc2715112ddc99b04d71716c69c3
Christian Friedow <christian@friedow.com>
2017-12-28 17:40:55 +0100

    Added content length and minimal reactions filter

--- postfilter.py
+++ postfilter.py
@@ -6,28 +6,37 @@ from functools import reduce
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Filter crawled facebook reactions.')
+    parser.add_argument('-t, --reaction-gap', dest='top_reaction_min_gap', action='store', default=10, type=int, metavar='top_reaction_min_gap', help='Filter posts where the dominant isnt X percent higher then the secondary reaction.')
     parser.add_argument('filename', metavar='filename', help='a json file [{"id": xxxx, "name": "page_name"}]')
     return parser.parse_args()
 
 
 def post_is_useful(post):
+    #Filter based on post content
     # Filter posts including URLs
     url_regex = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
     if re.match(url_regex, post.message):
         return False
+
+    # TODO: Filter posts with less than X characters
+    if post.message.length() < 20:
+        return False
     
+    # Filter based on reactions without likes
+    reactions = post.reactions.remove('like').values()
+
     # Filter posts where the highest reaction is not dominant. 
     # Default: 10% higher then the secondary reaction.
     # TODO: make percentage a command line argument
-    reactions = post.reactions.values()
     highgest_reaction = max(reactions)
     reactions.remove(highgest_reaction)
     second_highgest_reaction = max(reactions)
     if (100 - 100 * second_highgest_reaction / highgest_reaction) < 10:
         return False
 
-    # TODO: Filter posts with less than X reactions
-    # TODO: Filter posts with less than X characters
+    # TODO: Filter posts with less than X reactions 
+    if sum(reactions) < 100:
+        return False
 
     return True
 
