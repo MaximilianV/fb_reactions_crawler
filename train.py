0c76fe2078df95d4b8387e31684a2175206a4bc4
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-05 11:27:40 +0100

    add logging to train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import json
 from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.externals import joblib
+import logging
 
 
 def parse_arguments():
@@ -12,17 +13,33 @@ def parse_arguments():
 
 
 def main(run_args):
+    #Setup logger
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)-5.5s]  %(message)s",
+        handlers=[
+            logging.FileHandler("{0}/{1}.log".format("logs", "training")),
+            logging.StreamHandler()
+        ])
+
     filename = run_args.filename
     
     posts = None
+    logging.debug("Loading posts from " + filename)
     with open(filename, 'r') as infile:
         posts = json.load(infile)
-    
+
+    logging.debug("Converting posts to corpus array.")
     corpus = map(lambda post: post['message'], posts)
+
+    logging.debug("Extracting BoW vectors.")
     vectorizer = CountVectorizer()
     features = vectorizer.fit_transform(corpus)
-    print(features)
+
+    logging.debug("Persisting vectorizer for later use.")
     joblib.dump(vectorizer, "data/vectorizer.pkl")
+
+    logging.debug("Training model.")
     clf = svm.SVC()
     clf.fit(features, corpus)
 
