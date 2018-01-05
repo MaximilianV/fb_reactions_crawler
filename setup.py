5e5b310a47617a8174c5489134c5f70da0c1f5be
Christian Friedow <christian@friedow.com>
2018-01-05 13:06:37 +0100

    Added a setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import pip
+
+def installDependencies():
+    pip.main(['install', '-r', 'requirements.txt'])
+
+# Example
+if __name__ == '__main__':
+    installDependencies()
+    import nltk
+    nltk.download('punkt')
+    nltk.download('stopwords')
