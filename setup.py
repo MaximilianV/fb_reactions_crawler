83efc5f924bcc796e7802dc7130814e8ea1a5ece
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-07 13:51:56 +0100

    rename function corresponding to PEP8

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import pip
 
 
-def installDependencies():
+def install_dependencies():
     pip.main(['install', '-r', 'requirements.txt'])
 
 
-# Example
 if __name__ == '__main__':
-    installDependencies()
+    install_dependencies()
     import nltk
     nltk.download('punkt')
     nltk.download('stopwords')
