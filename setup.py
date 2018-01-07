6999dc3efe7a96fdf9f0e60912d3645b66ea70e1
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-07 13:52:18 +0100

    add function to initialize folder structure to setup script

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 import pip
+import pathlib
 
 
 def install_dependencies():
     pip.main(['install', '-r', 'requirements.txt'])
+
+
+def initialize_folderstructure():
+    print("Initializing required folder structure...", end="")
+    pathlib.Path('data/datasets').mkdir(parents=True, exist_ok=True)
+    pathlib.Path('data/models').mkdir(parents=True, exist_ok=True)
+    pathlib.Path('logs').mkdir(parents=True, exist_ok=True)
+    print(" Finished.")
 
 
 if __name__ == '__main__':
@@ -11,3 +20,4 @@ if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('stopwords')
     nltk.download('perluniprops')
+    initialize_folderstructure()
