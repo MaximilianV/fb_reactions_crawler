c396b5bf24c5717c088b00b8b95db8118c218d5a
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-08 11:32:36 +0100

    add naives bayes model

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 from training.svm_model import SvmModel
+from training.naivebayes_model import NaiveBayesModel
 from training.feature.features import Features
 
 
@@ -11,15 +12,20 @@ def parse_arguments():
 
 def main(run_args):
     # Train a SVM model with tfidf feature
-    svm_model = SvmModel()
-    svm_model.select_features([Features.Wordcount])
-    svm_model.train_from_file(run_args.filename)
-    svm_model.persist()
+    # model = SvmModel()
+
+    # Train a Naive Bayes model with tfidf feature
+    # See: http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+    model = NaiveBayesModel()
+
+    model.select_features([Features.Tfidf])
+    model.train_from_file(run_args.filename)
+    model.persist()
 
     while True:
-        doc = input("What do you want me to analyse?")
-        classification = svm_model.predict(doc)
-        print(str(classification) + " = " + svm_model.translate_reaction_id(classification[0]))
+        doc = input("What do you want me to analyse?\n")
+        classification = model.predict(doc)
+        print(str(classification) + " = " + model.translate_reaction_id(classification[0]))
 
 
 if __name__ == "__main__":
