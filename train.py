96760e3acc7dfc474632387ea271bf7e88f376e8
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-12 15:21:44 +0100

    adjust train script to ModelManager

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import argparse
 from training.svm_model import SvmModel
 from training.naivebayes_model import NaiveBayesModel
 from training.feature.features import Features
+from training.modelManager import ModelManager
 
 
 def parse_arguments():
@@ -18,14 +19,15 @@ def main(run_args):
     # See: http://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
     model = NaiveBayesModel()
 
-    model.select_features([Features.Tfidf])
+    model.select_features([Features.TfidfFeature])
     model.train_from_file(run_args.filename)
-    model.persist()
+    model_manager = ModelManager()
+    model_manager.save(model)
 
-    while True:
+    """while True:
         doc = input("What do you want me to analyse?\n")
         classification = model.predict(doc)
-        print(str(classification) + " = " + model.translate_reaction_id(classification[0]))
+        print(str(classification) + " = " + model.translate_reaction_id(classification[0]))"""
 
 
 if __name__ == "__main__":
